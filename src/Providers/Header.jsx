df3583efedc36a3a964feded132fbf4c88445a47
lucasdelmonte <delmontelucas678@gmail.com>
2021-11-22 17:15:49 -0300

    The ABM for suppliers was created (without redux)

---
+++ src/Providers/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { AddProvider } from './AddProvider';
+import { EditProvider } from './EditProvider';
+
+export const Header = (props) => {
+  const {
+    showProviderForm,
+    setShowProviderForm,
+    onAddProvider,
+    providerToEdit,
+    onEditProvider
+  } = props;
+  return (
+    <div>
+      <h1>Providers</h1>
+      <button
+        className="btn btn-primary"
+        onClick={() =>
+          setShowProviderForm({ show: !showProviderForm.show, mode: 'Add' })
+        }
+      >
+        {showProviderForm.show ? 'Cancel' : 'Add Provider'}
+      </button>
+
+      {showProviderForm.show ? (
+        showProviderForm.mode === 'Add' ? (
+          <AddProvider onAddProvider={onAddProvider} />
+        ) : (
+          <EditProvider
+            onEditProvider={onEditProvider}
+            providerToEdit={providerToEdit}
+          />
+        )
+      ) : undefined}
+    </div>
+  );
+};
---
+++ src/Providers/ProviderForm.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+
+export const ProviderForm = (props) => {
+  const { onSubmitForm, provider } = props;
+  const [name, setName] = useState(provider ? provider.name : '');
+  const [telephone, setTelephone] = useState(
+    provider ? provider.telephone : ''
+  );
+  const [location, setLocation] = useState(provider ? provider.location : '');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmitForm({
+      id: provider ? provider.id : undefined,
+      name,
+      telephone,
+      location
+    });
+    setName('');
+    setTelephone('');
+    setLocation('');
+  };
+
+  useEffect(() => {
+    setName(provider ? provider.name : '');
+    setTelephone(provider ? provider.telephone : '');
+    setLocation(provider ? provider.location : '');
+  }, [provider]);
+
+  return (
+    <form onSubmit={handleSubmit} className="mt-3">
+      <input
+        type="text"
+        name="name"
+        placeholder="Name"
+        className="form-control mb-3"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+      />
+      <input
+        type="text"
+        name="telephone"
+        placeholder="Telephone"
+        className="form-control mb-3"
+        onChange={(e) => setTelephone(e.target.value)}
+        value={telephone}
+      />
+      <input
+        type="number"
+        name="location"
+        placeholder="Location"
+        className="form-control mb-3"
+        onChange={(e) => setLocation(e.target.value)}
+        value={location}
+      />
+      <button type="submit">Confirm</button>
+    </form>
+  );
+};

d27b4f2e7cafd6cc0432e0b487af17589afd0b01
lucasdelmonte <delmontelucas678@gmail.com>
2021-11-23 16:26:35 -0300

    The functionality to edit suppliers with redux was implemented

--- src/Providers/AddProvider.jsx
+++ src/Providers/AddProvider.jsx
@@ -2,8 +2,5 @@ import React from 'react';
 import { ProviderForm } from './ProviderForm';
 
 export const AddProvider = (props) => {
-  //const { onAddProvider } = props;
-
   return <ProviderForm type="add" />;
-  //onSubmitForm={handleAdd}
 };

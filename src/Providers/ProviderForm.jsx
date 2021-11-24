654b126b5abd0637169787071092f17dcb75cc65
lucasdelmonte <delmontelucas678@gmail.com>
2021-11-24 11:47:36 -0300

    Libraries for working with modals and bootstrap buttons have been installed. In addition, this modality was implemented in suppliers

--- src/Providers/ProviderForm.jsx
+++ src/Providers/ProviderForm.jsx
@@ -4,7 +4,9 @@ import {
   creatorAddProvider,
   creatorEditProvider
 } from '../redux/actions/providersActions';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Modal from 'react-bootstrap/Modal';
+import { Button } from 'react-bootstrap';
 export const ProviderForm = (props) => {
   const dispatch = useDispatch();
   const { type, provider } = props;
@@ -13,7 +15,9 @@ export const ProviderForm = (props) => {
     provider ? provider.telephone : ''
   );
   const [location, setLocation] = useState(provider ? provider.location : '');
-
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   const handleSubmit = (event) => {
     event.preventDefault();
     if (type === 'add') {
@@ -30,42 +34,61 @@ export const ProviderForm = (props) => {
     setTelephone('');
     setLocation('');
   };
-
   useEffect(() => {
     setName(provider ? provider.name : '');
     setTelephone(provider ? provider.telephone : '');
     setLocation(provider ? provider.location : '');
   }, [provider]);
-
   return (
-    <form onSubmit={handleSubmit} className="mt-3">
-      <input
-        type="text"
-        name="name"
-        placeholder="Name"
-        className="form-control mb-3"
-        onChange={(e) => setName(e.target.value)}
-        value={name}
-      />
-      <input
-        type="number"
-        name="telephone"
-        placeholder="Telephone"
-        className="form-control mb-3"
-        onChange={(e) => setTelephone(e.target.value)}
-        value={telephone}
-      />
-      <input
-        type="text"
-        name="location"
-        placeholder="Location"
-        className="form-control mb-3"
-        onChange={(e) => setLocation(e.target.value)}
-        value={location}
-      />
-      <button className="btn btn-success" type="submit">
-        Confirm
-      </button>
-    </form>
+    <>
+      <Button
+        variant="primary"
+        onClick={handleShow}
+        className="btn btn-info ms-2"
+      >
+        Launch modal
+      </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Provider modal</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form onSubmit={handleSubmit} className="mt-3">
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              className="form-control mb-3"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+            />
+            <input
+              type="number"
+              name="telephone"
+              placeholder="Telephone"
+              className="form-control mb-3"
+              onChange={(e) => setTelephone(e.target.value)}
+              value={telephone}
+            />
+            <input
+              type="text"
+              name="location"
+              placeholder="Location"
+              className="form-control mb-3"
+              onChange={(e) => setLocation(e.target.value)}
+              value={location}
+            />
+            <button className="btn btn-success" type="submit">
+              Confirm
+            </button>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button className="btn btn-danger" onClick={handleClose}>
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };

ce005ba990ab8da0ad1cb032013644da29b7b06b
lucasdelmonte <delmontelucas678@gmail.com>
2021-11-23 21:39:47 -0300

    Provider content was put together in a single table and styled with CSS

--- src/Providers/ProviderList.jsx
+++ src/Providers/ProviderList.jsx
@@ -4,15 +4,27 @@ import { ProviderItem } from './ProviderItem';
 export const ProvidersList = (props) => {
   const { providerList, onDelete, onEdit } = props;
   return (
-    <div>
-      {providerList.map((provider) => (
-        <ProviderItem
-          key={provider.id}
-          providerToShow={provider}
-          onDelete={onDelete}
-          onEdit={onEdit}
-        />
-      ))}
-    </div>
+    <>
+      <div className="table-responsive">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Telephone</th>
+              <th>Location</th>
+              <th>Options</th>
+            </tr>
+          </thead>
+          {providerList.map((provider) => (
+            <ProviderItem
+              key={provider.id}
+              providerToShow={provider}
+              onDelete={onDelete}
+              onEdit={onEdit}
+            />
+          ))}
+        </table>
+      </div>
+    </>
   );
 };

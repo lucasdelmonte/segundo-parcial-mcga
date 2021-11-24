bd54eda54b325954fb1b047064944b3c21c87f5c
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-11-24 02:27:44 -0300

    products objects are read from json file

--- src/Products/EditProduct.jsx
+++ src/Products/EditProduct.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { ProductForm } from './ProductForm';
 
 export const EditProduct = (prop) => {
-  const { onEditProduct, productToEdit } = prop;
-  return <ProductForm type='edit' product={productToEdit} />;
+	const { productToEdit } = prop;
+	return <ProductForm type="edit" product={productToEdit} />;
 };
--- src/redux/reducers/productsReducer.js
+++ src/redux/reducers/productsReducer.js
@@ -1,49 +1,34 @@
-import {
-  ADD_PRODUCT,
-  REMOVE_PRODUCT,
-  EDIT_PRODUCT
-} from '../../constants/productsTypes';
+import { ADD_PRODUCT, REMOVE_PRODUCT, EDIT_PRODUCT } from '../../constants/productsTypes';
+
+import productMock from '../../mocks/products.json';
 
 const initialState = {
-  list: [
-    {
-      id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
-      name: 'Notebook ASUS',
-      description: 'SSD',
-      price: '178950'
-    },
-    {
-      id: '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed',
-      name: 'Mouse Logitech',
-      description: 'wireless',
-      price: '1750'
-    }
-  ],
-  isLoading: false,
-  productoSeleccionado: null,
-  error: ''
+	list: productMock.products,
+	isLoading: false,
+	productoSeleccionado: null,
+	error: '',
 };
 
 export const productsReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_PRODUCT:
-      return {
-        ...state,
-        list: [action.payload, ...state.list]
-      };
-    case REMOVE_PRODUCT:
-      return {
-        ...state,
-        list: state.list.filter((product) => product.id !== action.payload)
-      };
-    case EDIT_PRODUCT:
-      return {
-        ...state,
-        list: state.list.map((product) =>
-          product.id === action.payload.id ? action.payload : product
-        )
-      };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case ADD_PRODUCT:
+			return {
+				...state,
+				list: [action.payload, ...state.list],
+			};
+		case REMOVE_PRODUCT:
+			return {
+				...state,
+				list: state.list.filter((product) => product.id !== action.payload),
+			};
+		case EDIT_PRODUCT:
+			return {
+				...state,
+				list: state.list.map((product) =>
+					product.id === action.payload.id ? action.payload : product
+				),
+			};
+		default:
+			return state;
+	}
 };
